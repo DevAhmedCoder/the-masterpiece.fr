4bc7cb91c542df6cbe46ca381013a73f51aa82d9
devahmedcoder <ahmed.development.engineer@outlook.com>
2021-10-28 22:20:41 +0100

    Updates

---
+++ static/js/main.9df9ca83.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpthemasterpiece=this.webpackJsonpthemasterpiece||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(0),s=n.n(c),r=n(3),i=n.n(r),a=(n(8),n(1)),o=function(){return Object(a.jsx)("div",{className:"App",children:"Hello Ghassen"})};i.a.render(Object(a.jsx)(s.a.StrictMode,{children:Object(a.jsx)(o,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.9df9ca83.chunk.js.map
